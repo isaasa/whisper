5ef7106e3260f38dca4dc3ee6f0457a90678fa9f
isaasa <91382829+isaasa@users.noreply.github.com>
2025-02-05 16:14:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 apt update
 apt install ffmpeg -y
 apt install python3-pip
+apt install python3.12-venv
+python3 -m venv path/to/venv
+source path/to/venv/bin/activate
 pip3 install -r requirements.txt
+cd ..
